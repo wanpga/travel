c57d7068be19d43673b5cf29ba1447812b523654
wanpga <16630016+wanpga@users.noreply.github.com>
2026-05-16 11:50:06 -0400

    add initial travel test page

---
+++ js/test.js
@@ -0,0 +1,5 @@
+window.travelTest = {
+  test: function () {
+    return true;
+  }
+};
---
+++ sw.js
@@ -0,0 +1,37 @@
+var CACHE_NAME = 'travel-pwa-v1';
+var urlsToCache = [
+  '/travel/',
+  '/travel/index.html',
+  '/travel/js/test.js',
+  '/travel/manifest.json'
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function (name) {
+          return name !== CACHE_NAME;
+        }).map(function (name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+});
